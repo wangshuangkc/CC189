7f317bed7e9b54dd2b42f5a9aa357c7a3fa8d457
wangshuangkc <sw2553@nyu.edu>
2018-02-25 23:20:37 -0800

    [CC189] Add test cases

--- src/chapter1/Is_Unique.java
+++ src/chapter1/Is_Unique.java
@@ -10,13 +10,14 @@ import java.util.Map;
  *
  * Questions
  * 1. ASCII (128 or 256) or UniCode
- * 2. Time and space complexity
- * 3. Modify the string
+ * 2. Lower and upper cases
+ * 3. Time and space complexity
+ * 4. Modify the string
  */
 public class Is_Unique {
 
   // Time: O(N) Space: O(N)
-  public boolean isUnique_hashTable(String str) {
+  public static boolean isUnique_hashTable(String str) {
     Map<Character, Character> map = new HashMap<>();
     for (char s : str.toCharArray()) {
       if(map.containsKey(s)) {
@@ -30,7 +31,7 @@ public class Is_Unique {
   }
 
   // Time: O(N) Space: O(1)
-  public boolean isUnique_ascii128(String str) {
+  public static boolean isUnique_ascii128(String str) {
     if (str.length() > 128) {
       return false;
     }
@@ -49,7 +50,7 @@ public class Is_Unique {
   }
 
   // Time: O(N^2) Space: O(1)
-  public boolean isUnique_noDataStructure(String str) {
+  public static boolean isUnique_noDataStructure(String str) {
     char[] chars = str.toCharArray();
     for (int i = 0; i < chars.length; i++) {
       char c = chars[i];
@@ -63,31 +64,57 @@ public class Is_Unique {
   }
 
   // Time: O(N) Space: O(N)
-  public boolean isUnique_bitVector(String str) {
+  // Assume only lower cases
+  public static boolean isUnique_bitVector(String str) {
     int checker = 0;
     for (int i = 0; i < str.length(); i++) {
       int val = str.charAt(i) - 'a';
       if ((checker & (1 << val)) > 0) {
         return false;
       }
-      checker |= 1 << val;
+      checker |= (1 << val);
     }
 
     return true;
   }
 
   // Time: O(NlogN) Space: O(N)
-  public boolean isUnique_sorted(String str) {
+  public static boolean isUnique_sorted(String str) {
     char[] chars = str.toCharArray();
     // ToDo sort in O(NlogN) without extra space
     Arrays.sort(chars);
 
-    for (int i = 0; i < chars.length; i++) {
+    for (int i = 0; i < chars.length - 1; i++) {
       if (chars[i] == chars[i + 1]) {
         return false;
       }
     }
 
     return true;
+  }
+
+  public static void main(String[] args) {
+    String word1 = "Uniqe";
+    String word3 = "Not unique";
+
+    System.out.println(String.format("isUnique_hashTable: \n%s: %s | %s: %s\n",
+        word1, isUnique_hashTable(word1),
+        word3, isUnique_hashTable(word3)));
+
+    System.out.println(String.format("isUnique_ascii128: \n%s: %s | %s: %s\n",
+        word1, isUnique_ascii128(word1),
+        word3, isUnique_ascii128(word3)));
+
+    System.out.println(String.format("isUnique_bitVector: \n%s: %s | %s: %s\n",
+        word1.toLowerCase(), isUnique_bitVector(word1.toLowerCase()),
+        word3.toLowerCase(), isUnique_bitVector(word3.toLowerCase())));
+
+    System.out.println(String.format("isUnique_noDataStructure: \n%s: %s | %s: %s\n",
+        word1, isUnique_noDataStructure(word1),
+        word3, isUnique_noDataStructure(word3)));
+
+    System.out.println(String.format("isUnique_sorted: \n%s: %s | %s: %s\n",
+        word1, isUnique_sorted(word1),
+        word3, isUnique_sorted(word3)));
   }
 }
